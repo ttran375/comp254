64a2b3696c4f2fab0a2edfc47fa188e5a86839ee
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-10 18:08:39 +0000

    May 10, 2024, 6:08 PM

---
+++ Week01/java/Week01/StudentDemo.java
@@ -0,0 +1,8 @@
+package Week01;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student1 = new Student(30102323, "Salima", "email@my.centennialcollege.ca");
+        System.out.println(student1);
+    }
+}
