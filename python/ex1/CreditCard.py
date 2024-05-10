ef048530670ae171647eb2a97d79152c4971836b
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-10 19:13:16 +0000

    Fri, May 10, 2024, 7:13 PM +00:00

---
+++ python/ex1/CreditCard.py
@@ -0,0 +1,41 @@
+class CreditCard:
+    def __init__(self, cust, bk, acnt, lim, initialBal=0.0):
+        self.customer = cust
+        self.bank = bk
+        self.account = acnt
+        self.limit = lim
+        self.balance = initialBal
+
+    def getCustomer(self):
+        return self.customer
+
+    def getBank(self):
+        return self.bank
+
+    def getAccount(self):
+        return self.account
+
+    def getLimit(self):
+        return self.limit
+
+    def getBalance(self):
+        return self.balance
+
+    def charge(self, price):
+        if price + self.balance > self.limit:
+            return False
+        self.balance += price
+        return True
+
+    def makePayment(self, amount):
+        self.balance -= amount
+
+    @staticmethod
+    def printSummary(card):
+        print("Customer =", card.customer)
+        print("Bank =", card.bank)
+        print("Account =", card.account)
+        print("Balance =", card.balance)
+        print("Limit =", card.limit)
+
+
---
+++ python/ex1/PredatoryCreditCard.py
@@ -0,0 +1,47 @@
+from CreditCard import CreditCard
+
+class PredatoryCreditCard(CreditCard):
+    def __init__(self, cust, bk, acnt, lim, initialBal, rate):
+        super().__init__(cust, bk, acnt, lim, initialBal)
+        self.apr = rate
+
+    def processMonth(self):
+        if self.balance > 0:
+            monthlyFactor = (1 + self.apr) ** (1.0 / 12)
+            self.balance *= monthlyFactor
+
+    def charge(self, price):
+        isSuccess = super().charge(price)
+        if not isSuccess:
+            self.balance += 5
+        return isSuccess
+
+
+def main():
+    wallet = [
+        CreditCard("John Bowman", "California Savings", "5391 0375 9387 5309", 5000),
+        CreditCard("John Bowman", "California Federal", "3485 0399 3395 1954", 3500),
+        CreditCard("John Bowman", "California Finance", "5391 0375 9387 5309", 2500, 300)
+    ]
+
+    for val in range(1, 17):
+        wallet[0].charge(3 * val)
+        wallet[1].charge(2 * val)
+        wallet[2].charge(val)
+
+    for card in wallet:
+        CreditCard.printSummary(card)
+        while card.getBalance() > 200.0:
+            card.makePayment(200)
+            print("New balance =", card.getBalance())
+
+    predatory_card = PredatoryCreditCard("Michael", "Payday", "1111 1111 1111 1111", 2500, 0, 0.0825)
+    predatory_card.charge(100)
+    CreditCard.printSummary(predatory_card)
+    predatory_card.processMonth()
+    CreditCard.printSummary(predatory_card)
+    predatory_card.charge(10000)
+    CreditCard.printSummary(predatory_card)
+
+if __name__ == "__main__":
+    main()
