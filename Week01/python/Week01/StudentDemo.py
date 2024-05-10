9dda5cbe09877ff25a1051691a5816278ef23703
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-10 18:13:31 +0000

    May 10, 2024, 6:13 PM

--- Week01/python/Week01/StudentDemo.py
+++ Week01/python/Week01/StudentDemo.py
@@ -1,10 +1,10 @@
 from Student import Student
 
-class StudentDemo:
-    @staticmethod
-    def main():
-        student1 = Student(30102323, "Salima", "email@my.centennialcollege.ca")
-        print(student1)
 
-# Call the main method
-StudentDemo.main()
+def main():
+    student1 = Student(30102323, "Salima", "email@my.centennialcollege.ca")
+    print(student1)
+
+
+if __name__ == "__main__":
+    main()
