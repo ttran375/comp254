02370db750a84b81dce89c98463d125e31356c8e
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-24 18:50:23 +0000

    Fri, May 24, 2024, 6:50 PM +00:00

--- DemoClass.java
+++ DemoClass.java
@@ -1,9 +1,23 @@
+import java.util.Arrays;
+
 public class DemoClass {
     public static void main(String[] args) {
+        // int[] data = { 12 };
+        // int[] backup;
+        // backup = data;
+        // data[0] = 11;
+        // System.out.println(backup[0]);
+
+        // System.out.println(data == backup);
+
+        // int[] data = { 12 };
+        // int[] backup = new int[1];
+        // data[0] = 11;
+        // System.out.println(data.equals(backup));
+
         int[] data = { 12 };
-        int[] backup;
-        backup = data;
-        data[0] = 11;
-        System.out.println(backup[0]);
+        int[] backup = new int[1];
+        backup[0] = 12;
+        System.out.println(Arrays.equals(data, backup));
     }
 }
