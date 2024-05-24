75669eadaacdb4ce87cdabb57cfe35c9b3e25251
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-24 19:10:24 +0000

    Fri, May 24, 2024, 7:10 PM +00:00

--- DemoClass.java
+++ DemoClass.java
@@ -19,15 +19,60 @@ public class DemoClass {
         // backup[0] = 12;
         // System.out.println(Arrays.equals(data, backup));
 
+        // int[][] data = { { 1, 2 }, { 3, 4 } };
+        // int[][] backup = new int[2][2];
+        // backup[0][0] = 1;
+        // backup[0][1] = 2;
+        // backup[1][0] = 3;
+        // backup[1][1] = 4;
+        // System.out.println(data == backup);
+        // System.out.println(data.equals(backup));
+        // System.out.println(Arrays.equals(data, backup));
+        // System.out.println(Arrays.deepEquals(data, backup));
+
+        // int[] data = { 1, 2 };
+        // int[] backup;
+        // backup = data;
+        // data[0] = 11;
+        // System.out.println(backup[0]);
+
+        // System.out.println(data == backup);
+        // System.out.println(data.equals(backup));
+        // System.out.println(Arrays.equals(data, backup));
+        // // System.out.println(Arrays.deepEquals(data, backup));
+
+        // int[] data = { 1, 2 };
+        // int[] backup;
+        // backup = data.clone();
+        // data[0] = 11;
+        // System.out.println(backup[0]);
+
+        // System.out.println(data == backup);
+        // System.out.println(data.equals(backup));
+        // System.out.println(Arrays.equals(data, backup));
+        // // System.out.println(Arrays.deepEquals(data, backup));
+
+        // int[][] data = { { 1, 2 }, { 3, 4 } };
+        // int[][] backup;
+        // backup = data.clone();
+        // data[0][0] = 11;
+        // System.out.println(backup[0][0]);
+
+        // System.out.println(data == backup);
+        // System.out.println(data.equals(backup));
+        // System.out.println(Arrays.equals(data, backup));
+        // // System.out.println(Arrays.deepEquals(data, backup));
+
         int[][] data = { { 1, 2 }, { 3, 4 } };
-        int[][] backup = new int[2][2];
-        backup[0][0] = 1;
-        backup[0][1] = 2;
-        backup[1][0] = 3;
-        backup[1][1] = 4;
+        int[][] backup = new int[data.length][];
+        backup[0] = data[0].clone();
+        backup[1] = data[1].clone();
+        data[0][0] = 11;
+        System.out.println(backup[0][0]);
+
         System.out.println(data == backup);
         System.out.println(data.equals(backup));
         System.out.println(Arrays.equals(data, backup));
-        System.out.println(Arrays.deepEquals(data, backup));
+        // System.out.println(Arrays.deepEquals(data, backup));
     }
 }
