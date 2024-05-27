bd0d936691cad0a1a2f47e5abaa242e7164bb8e6
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-27 18:08:33 +0000

    Mon, May 27, 2024, 6:08 PM +00:00

--- StringExperiment.java
+++ StringExperiment.java
@@ -56,53 +56,57 @@ public class StringExperiment {
    * first command line argument can be used to change the number of
    * trials, and the second to adjust the start value.
    */
-  // public static void main(String[] args) {
-  // int n = 50000; // starting value
-  // int trials = 10;
-  // try {
-  // if (args.length > 0)
-  // trials = Integer.parseInt(args[0]);
-  // if (args.length > 1)
-  // n = Integer.parseInt(args[1]);
-  // } catch (NumberFormatException e) { }
-  // int start = n; // remember the original starting value
-
-  // // let's run version 2 (the quicker one) first
-  // System.out.println("Testing repeat2...");
-  // for (int t=0; t < trials; t++) {
-  // long startTime = System.currentTimeMillis();
-  // String temp = repeat2('-', n);
-  // long endTime = System.currentTimeMillis();
-  // long elapsed = endTime - startTime;
-  // System.out.println(String.format("n: %9d took %12d milliseconds", n,
-  // elapsed));
-  // n *= 2; // double the problem size
-  // }
-
-  // System.out.println("Testing repeat1...");
-  // n = start; // restore n to its start value
-  // for (int t=0; t < trials; t++) {
-  // long startTime = System.currentTimeMillis();
-  // String temp = repeat1('-', n);
-  // long endTime = System.currentTimeMillis();
-  // long elapsed = endTime - startTime;
-  // System.out.println(String.format("n: %9d took %12d milliseconds", n,
-  // elapsed));
-  // n *= 2; // double the problem size
-  // }
-  // }
-
   public static void main(String[] args) {
-    long startTime = System.currentTimeMillis();
-    String temp = repeat1('-', 10000);
-    long endTime = System.currentTimeMillis();
-    long elapsed = endTime - startTime;
-    System.out.println(String.format("n: %9d took %12d milliseconds", 10000, elapsed));
+    int n = 50000; // starting value
+    int trials = 10;
+    try {
+      if (args.length > 0)
+        trials = Integer.parseInt(args[0]);
+      if (args.length > 1)
+        n = Integer.parseInt(args[1]);
+    } catch (NumberFormatException e) {
+    }
+    int start = n; // remember the original starting value
 
-    long startTime1 = System.currentTimeMillis();
-    String temp1 = repeat2('-', 10000);
-    long endTime1 = System.currentTimeMillis();
-    long elapsed1 = endTime1 - startTime1;
-    System.out.println(String.format("Algorithm 2 n: %9d took %12d milliseconds", 10000, elapsed1));
+    // let's run version 2 (the quicker one) first
+    System.out.println("Testing repeat2...");
+    for (int t = 0; t < trials; t++) {
+      long startTime = System.currentTimeMillis();
+      String temp = repeat2('-', n);
+      long endTime = System.currentTimeMillis();
+      long elapsed = endTime - startTime;
+      System.out.println(String.format("n: %9d took %12d milliseconds", n,
+          elapsed));
+      n *= 2; // double the problem size
+    }
+
+    // System.out.println("Testing repeat1...");
+    // n = start; // restore n to its start value
+    // for (int t=0; t < trials; t++) {
+    // long startTime = System.currentTimeMillis();
+    // String temp = repeat1('-', n);
+    // long endTime = System.currentTimeMillis();
+    // long elapsed = endTime - startTime;
+    // System.out.println(String.format("n: %9d took %12d milliseconds", n,
+    // elapsed));
+    // n *= 2; // double the problem size
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // long startTime = System.currentTimeMillis();
+    // String temp = repeat1('-', 10000);
+    // long endTime = System.currentTimeMillis();
+    // long elapsed = endTime - startTime;
+    // System.out.println(String.format("n: %9d took %12d milliseconds", 10000,
+    // elapsed));
+
+    // long startTime1 = System.currentTimeMillis();
+    // String temp1 = repeat2('-', 10000);
+    // long endTime1 = System.currentTimeMillis();
+    // long elapsed1 = endTime1 - startTime1;
+    // System.out.println(String.format("Algorithm 2 n: %9d took %12d milliseconds",
+    // 10000, elapsed1));
+    // }
   }
 }
