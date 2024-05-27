a81f4fd833b14d64a0c40893034f68140b458990
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-27 18:53:21 +0000

    Mon, May 27, 2024, 6:53 PM +00:00

--- GenericMethodTest.java
+++ GenericMethodTest.java
@@ -4,9 +4,9 @@
 public class GenericMethodTest {
    public static void main(String[] args) {
       // create arrays of Integer, Double and Character
-      Integer[] integerArray = {1, 2, 3, 4, 5};
-      Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
-      Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+      Integer[] integerArray = { 1, 2, 3, 4, 5 };
+      Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+      Character[] characterArray = { 'H', 'E', 'L', 'L', 'O' };
 
       System.out.printf("Array integerArray contains: ");
       printArray(integerArray); // pass an Integer array
@@ -16,29 +16,28 @@ public class GenericMethodTest {
       printArray(characterArray); // pass a Character array
    }
 
-   // generic method printArray                     
+   // generic method printArray
    public static <T> void printArray(T[] inputArray) {
-      // display array elements            
-      for (T element : inputArray) {       
+      // display array elements
+      for (T element : inputArray) {
          System.out.printf("%s ", element);
-      }                                    
+      }
 
       System.out.println();
-   } 
-} 
-
+   }
+}
 
 /**************************************************************************
- * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and *
+ * Pearson Education, Inc. All Rights Reserved. *
+ * *
+ * DISCLAIMER: The authors and publisher of this book have used their *
+ * best efforts in preparing the book. These efforts include the *
+ * development, research, and testing of the theories and programs *
+ * to determine their effectiveness. The authors and publisher make *
+ * no warranty of any kind, expressed or implied, with regard to these *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * and publisher shall not be liable in any event for incidental or *
+ * consequential damages in connection with, or arising out of, the *
+ * furnishing, performance, or use of these programs. *
  *************************************************************************/
