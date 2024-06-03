bdddda663877c3c4e8874444746f6f3045519953
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-03 19:15:26 +0000

    Mon, Jun 3, 2024, 7:15 PM +00:00

--- Factorial.java
+++ Factorial.java
@@ -32,11 +32,11 @@ public class Factorial {
 
   /** Computes the factorial of the given (nonnegative) integer) */
   public static int factorial(int n) throws IllegalArgumentException {
-    // if (n < 0)
-    //   throw new IllegalArgumentException();     // argument must be nonnegative
-    // else if (n == 0)
-    //   return 1;                                 // base case
-    // else
+    if (n < 0)
+      throw new IllegalArgumentException();     // argument must be nonnegative
+    else if (n == 0)
+      return 1;                                 // base case
+    else
       return n * factorial(n-1);                // recursive case
   }
 
