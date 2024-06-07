18090d8ec678d3ff443acdcd9d3d584c7066331f
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-07 18:24:58 +0000

    Fri, Jun 7, 2024, 6:24 PM +00:00

--- Factorial.java
+++ Factorial.java
@@ -32,23 +32,22 @@ public class Factorial {
 
   /** Computes the factorial of the given (nonnegative) integer) */
   public static int factorial(int n) throws IllegalArgumentException {
-    if (n < 0)
-      throw new IllegalArgumentException();     // argument must be nonnegative
-    else if (n == 0)
-      return 1;                                 // base case
-    else
+    // if (n < 0)
+    //   throw new IllegalArgumentException();     // argument must be nonnegative
+    // else if (n == 0)
+    //   return 1;                                 // base case
+    // else
       return n * factorial(n-1);                // recursive case
   }
 
   /** Simple test, assuming valid integer given as command-line argument */
   public static void main(String[] args) {
-    // if (args.length > 0) {
-    //   int n = Integer.parseInt(args[0]);
-    int n=5;
+    if (args.length > 0) {
+      int n = Integer.parseInt(args[0]);
       try { System.out.println("factorial("+n+") = " + factorial(n)); }
       catch (IllegalArgumentException e) {
         System.out.println("Error: the factorial function is undefined for negative integers"); }
     }
   }
 
-// }
+}
