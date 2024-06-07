d45b682dc92ef3d7a9594de1f6acf89a349c7406
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-07 15:36:38 -0400

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,75 +1,83 @@
+/*
+ * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
+ *
+ * Developed for use with the book:
+ *
+ *    Data Structures and Algorithms in Java, Sixth Edition
+ *    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+ *    John Wiley & Sons, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 /**
- * Demonstration of two recursive approaches to computing the sum of an array of
- * integers.
+ * Demonstration of two recursive approaches to computing the sum of an array of integers.
  *
  * @author Michael T. Goodrich
  * @author Roberto Tamassia
  * @author Michael H. Goldwasser
  */
 public class ArraySum {
-
+ 
   /** Returns the sum of the first n integers of the given array. */
   public static int linearSum(int[] data, int n) {
     if (n == 0)
       return 0;
     else
-      return linearSum(data, n - 1) + data[n - 1];
+      return linearSum(data, n-1) + data[n-1];
   }
-
+ 
+ 
   /** Returns the sum of subarray data[low] through data[high] inclusive. */
   public static int binarySum(int[] data, int low, int high) {
-    if (low > high) // zero elements in subarray
+    if (low > high)                 // zero elements in subarray
       return 0;
-    else if (low == high) // one element in subarray
+    else if (low == high)           // one element in subarray
       return data[low];
     else {
       int mid = (low + high) / 2;
-      return binarySum(data, low, mid) + binarySum(data, mid + 1, high);
+      return binarySum(data, low, mid) + binarySum(data, mid+1, high);
     }
   }
-
-  /** Returns the maximum value in the array. */
-  public static int findMax(int[] data, int n) {
-    if (n == 1) // Base case: only one element
-      return data[0];
-    else
-      return Math.max(data[n - 1], findMax(data, n - 1));
-  }
-
+ 
   public static void main(String[] args) {
-
-    // // int[] data = { 3, 4, 8, 6 ,2 };
-    // int max = findMax(data, data.length);
-    // System.out.println("The maximum value is: " + max);
-
-    // for (int limit = 1; limit < 100; limit++) {
-    // int[] data = new int[limit];
-    // for (int k = 0; k < limit; k++)
-    // data[k] = k + 1;
-    // int answer = limit * (limit + 1) / 2;
-
-    // int linear = linearSum(data, limit);
-    // if (linear != answer)
-    // System.out.println("Problem with linear sum for n=" + limit);
-
-    // int binary = binarySum(data, 0, limit - 1);
-    // if (binary != answer)
-    // System.out.println("Problem with binary sum for n=" + limit);
-    // }
-    int limit = 100000;
-    int[] data = new int[limit];
-    for (int k = 0; k < limit; k++)
-      data[k] = k + 1;
-
-    int answer = limit * (limit + 1) / 2;
-
-    int linear = linearSum(data, limit);
-    if (linear != answer)
-      System.out.println("Problem with linear sum for n=" + limit);
-
-    int binary = binarySum(data, 0, limit - 1);
-    if (binary != answer)
-      System.out.println("Problem with binary sum for n=" + limit);
-
+ 
+    //for (int limit = 1; limit < 100; limit++) {
+	  int limit=100000;
+      int[] data = new int[limit];
+ 
+      for (int k=0; k < limit; k++)
+        data[k] = k+1;
+      int answer = limit * (limit + 1) / 2;
+ 
+     /* int linear = linearSum(data, limit);
+ 
+      if (linear != answer)
+        System.out.println("Problem with linear sum for n=" + limit);*/
+ 
+      int binary = binarySum(data, 0, limit-1);
+      if (binary != answer)
+        System.out.println("Problem with binary sum for n=" + limit);
+    }
+ 
+ 
+private static int findMax(int[] data, int n) {
+	// TODO Auto-generated method stub
+	if(n==1)
+		return data[0];
+	else 
+		return Math.max(findMax(data,n-1),data[n-1]);
+	}
   }
-}
+ 
